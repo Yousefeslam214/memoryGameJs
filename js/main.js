7ad73d4d3dd9a58ad9d2798c2e593e43e8e4d5ec
yousefeslam <email@example.com>
2023-05-10 07:02:08 +0200

    shfle function

--- js/main.js
+++ js/main.js
@@ -17,3 +17,43 @@ document.querySelector(".control-buttons span").onclick = function () {
        // Remove Splash Screen
        document.querySelector(".control-buttons").remove();
 };
+
+//Effect Duration
+let duration = 1000;
+
+//Select Blocks Container
+let blocksContainer = document.querySelector(".memory-game-blocks");
+
+//create Array From Game Blocks
+let blocks = Array.from(blocksContainer.children);
+
+//Create Range of Keys
+let orderRange = [...Array(blocks.length).keys()];
+
+suffle(orderRange)
+blocks.forEach((block,index)=>{
+       block.style.order = orderRange[index]
+})
+
+function suffle(array) {
+       let current = array.length,
+       temp,random;
+       while (current > 0) {
+              //Get Random Number
+              random = Math.floor(Math.random() * current);
+              current--;
+              console.log(random)
+
+              // Save Curent Ele in Stash
+              temp = array[current];
+
+              //Current Ele = Random Ele
+              array[current] = array[random];
+
+              //Random Ele = Get Ele feom stash
+              array[random] = temp;
+
+       }
+
+       return array;
+}
